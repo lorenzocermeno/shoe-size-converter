14b4bd281882dc1d7aeac741f2de968e271110d4
Lorenzo Cermeno <lorenzocermeno@Lorenzos-MacBook-Air.local>
2023-04-10 19:23:36 +0200

    Added more tests

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,3 +1,4 @@
+import { IData } from "../interface/interface";
 import {
   convertGenderSynonym,
   isGenderMan,
@@ -5,9 +6,27 @@ import {
   isSizeFormatValid,
   isTypeValid,
   capitalizeFirstLetter,
+  isGenderAvailable,
+  getSizes,
 } from "../utils/utils";
 
 describe("src/utils/utils.ts", () => {
+  const mockData: IData = {
+    nike: {
+      cm: {
+        men: ["28", "29", "30"],
+        women: ["20", "21", "22"],
+      },
+    },
+  };
+
+  const gender = {
+    men: "men",
+    women: "women",
+    male: "male",
+    female: "female",
+  };
+
   describe("should capitalize first letter()", () => {
     test("should return the string with the first letter capitalized", () => {
       const string = "asd";
@@ -61,13 +80,6 @@ describe("src/utils/utils.ts", () => {
   });
 
   describe("isGenderManageable()", () => {
-    const manageableGender = {
-      men: "men",
-      women: "women",
-      male: "male",
-      female: "female",
-    };
-
     const unManageableGender = {
       kids: "kids",
       infant: "infant",
@@ -76,19 +88,19 @@ describe("src/utils/utils.ts", () => {
     };
 
     test("should return true when provided with 'men'", () => {
-      expect(isGenderManageable(manageableGender.men)).toBe(true);
+      expect(isGenderManageable(gender.men)).toBe(true);
     });
 
     test("should return true when provided with 'women'", () => {
-      expect(isGenderManageable(manageableGender.women)).toBe(true);
+      expect(isGenderManageable(gender.women)).toBe(true);
     });
 
     test("should return true when provided with 'male'", () => {
-      expect(isGenderManageable(manageableGender.male)).toBe(true);
+      expect(isGenderManageable(gender.male)).toBe(true);
     });
 
     test("should return true when provided with 'female'", () => {
-      expect(isGenderManageable(manageableGender.female)).toBe(true);
+      expect(isGenderManageable(gender.female)).toBe(true);
     });
 
     test("should return false when provided with 'kids'", () => {
@@ -109,13 +121,6 @@ describe("src/utils/utils.ts", () => {
   });
 
   describe("convertGenderSynonym()", () => {
-    const gender = {
-      men: "men",
-      women: "women",
-      male: "male",
-      female: "female",
-    };
-
     test("should return 'men' when provided with 'men'", () => {
       expect(convertGenderSynonym(gender.men)).toStrictEqual(gender.men);
     });
@@ -133,7 +138,30 @@ describe("src/utils/utils.ts", () => {
     });
   });
 
-  //TODO Add tests for isGenderAvailable()
+  describe("isGenderAvailable()", () => {
+    test("should return true when provided with 'men'", () => {
+      expect(isGenderAvailable(mockData, "nike", "cm", gender.men)).toBe(true);
+    });
+
+    test("should return true when provided with 'women'", () => {
+      expect(isGenderAvailable(mockData, "nike", "cm", gender.women)).toBe(
+        true
+      );
+    });
+
+    test("should return false when provided with 'female'", () => {
+      expect(isGenderAvailable(mockData, "nike", "cm", gender.female)).toBe(
+        false
+      );
+    });
+
+    test("should return false when provided with 'male'", () => {
+      expect(isGenderAvailable(mockData, "nike", "cm", gender.male)).toBe(
+        false
+      );
+    });
+  });
+
   describe("isGenderMan()", () => {
     const parameter = {
       men: "men",
@@ -150,4 +178,15 @@ describe("src/utils/utils.ts", () => {
   });
 
   //TODO Add tests for getSizes()
+  describe("getSizes()", () => {
+    test("should return '28, 29, 30' for men", () => {
+      const result = ["28", "29", "30"];
+      expect(getSizes(mockData, "nike", "cm", true)).toStrictEqual(result);
+    });
+
+    test("should return '21, 22, 23' for men", () => {
+      const result = ["20", "21", "22"];
+      expect(getSizes(mockData, "nike", "cm", false)).toStrictEqual(result);
+    });
+  });
 });
